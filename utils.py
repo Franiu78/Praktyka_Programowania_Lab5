b54f022f315ea5ed3cd2214703e13582732b1f82
Filip Sanowski <filipsanowski@gmail.com>
2026-05-28 17:53:28 +0200

    small fix

--- utils.py
+++ utils.py
@@ -1,9 +1,22 @@
+"""Simple calculator helper functions."""
+
+
 # Exemplary calculator functions
-def add (a: int , b: int) -> int:
+def add(a: int, b: int) -> int:
+    """Return the sum of two integers."""
     return a + b
-def subtract (a: int , b: int) -> int: 
+
+
+def subtract(a: int, b: int) -> int:
+    """Return the difference of two integers."""
     return a - b
-def multiply (a: int , b: int) -> int:
+
+
+def multiply(a: int, b: int) -> int:
+    """Return the product of two integers."""
     return a * b
-def divide (a: int , b: int) -> float :
+
+
+def divide(a: int, b: int) -> float:
+    """Return the quotient of two numbers as float."""
     return a / b
