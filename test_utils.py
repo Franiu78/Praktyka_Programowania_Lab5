0016f487001ce5ac4c485ffacc1182d9bac5ab06
Filip Sanowski <filipsanowski@gmail.com>
2026-05-28 18:11:55 +0200

    ADDED new binnary functionality

--- test_utils.py
+++ test_utils.py
@@ -29,3 +29,20 @@ def test_multiply(a, b, expected):
 def test_divide(a, b, expected):
     result = utils.divide(a, b)
     assert result == expected
+
+@pytest.mark.parametrize("a, expected", [(1, 1), (2, 10), (3, 11), (4, 100)])
+def test_bin(a, expected):
+    result = utils.bin(a)
+    assert result == expected
+
+
+@pytest.mark.parametrize("a", [-1, 101, 110])
+def test_bin_value_error(a):
+    with pytest.raises(ValueError):
+        utils.bin(a)   
+
+
+@pytest.mark.parametrize("a", [0.5, 1.5, 39.5])
+def test_bin_type_error(a):
+    with pytest.raises(TypeError):
+        utils.bin(a)
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@
 
 
 # Exemplary calculator functions
+import builtins
+
+
 def add(a: int, b: int) -> int:
     """Return the sum of two integers."""
     return a + b
@@ -20,3 +23,12 @@ def multiply(a: int, b: int) -> int:
 def divide(a: int, b: int) -> float:
     """Return the quotient of two numbers as float."""
     return a / b
+
+
+def bin(a: int) -> int:
+    """Return the binary representation of an integer."""
+    if not isinstance(a, int):
+        raise TypeError("Input must be an integer.")
+    if a < 0 or a > 100:
+        raise ValueError("Input must be between 0 and 100.")
+    return int(builtins.bin(a)[2:])
